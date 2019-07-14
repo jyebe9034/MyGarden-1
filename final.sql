dc14fe5b751dc3fe0d54bd5473a7d8edf6c83c74
Jonah <vsevolod117@gmail.com>
2019-07-15 00:55:01 +0900

    commit

--- final.sql
+++ final.sql
@@ -19,5 +19,7 @@ m_realpath varchar(300)
 );
 drop table members;
 select * from members;
+select m_pw from members where m_email = 'gmail@gmail';
+delete from members where m_Email = 'sparkss0419@naver.com';
 commit;
 select sysdate from dual;
