49d622c355e985ea6648fbed6b963bb36871658f
hanle <cwg94@naver.com>
2019-07-29 15:50:48 +0900

    hy

--- src/main/java/my/garden/schedule/ScheduleTask.java
+++ src/main/java/my/garden/schedule/ScheduleTask.java
@@ -3,6 +3,8 @@ package my.garden.schedule;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.Trigger;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.support.PeriodicTrigger;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 import my.garden.daoImpl.ShoppingDAOImpl;
 
 @Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class ScheduleTask {
 
 	private ThreadPoolTaskScheduler scheduler;
